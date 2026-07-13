ec9f120709165244831c773f2b713a3fda55bfaa
mani0258 <free.shachi@gmail.com>
2026-07-13 10:13:20 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='az104-v2';
+const CACHE='az104-v5';
 const ASSETS=['./','./index.html','./questions.json','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
